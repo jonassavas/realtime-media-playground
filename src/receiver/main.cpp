b84860506f7d32edbd677d6cdfe0b017bdf9167c
Jonas Sävås <jonas.savas123@gmail.com>
2026-06-08 18:17:14 +0200

    Adds comments describing the socket creation process in both the sender & receiver

--- src/receiver/main.cpp
+++ src/receiver/main.cpp
@@ -9,15 +9,22 @@
 int main()
 {
     WSADATA wsaData;
+    // Winsock version 2.2
     WSAStartup(MAKEWORD(2, 2), &wsaData);
 
+    // Adress Family Internet/IPv4, Datagram socket/UDP, Default protocol
     SOCKET sock = socket(AF_INET, SOCK_DGRAM, 0);
 
+    // Define where we want to listen for packets - Structure: IP Address + port
     sockaddr_in addr{};
-    addr.sin_family = AF_INET;
+    addr.sin_family = AF_INET; // IPv4
+    // PC (little-endian), Network (big-endian). 
+    // htons: host to network short, 5000 -> network byte order
     addr.sin_port = htons(5000);
+    // Listen on every local interface
     addr.sin_addr.s_addr = INADDR_ANY;
 
+    // Attatch this socket to port 5000
     bind(sock, (sockaddr*)&addr, sizeof(addr));
 
     std::cout << "Receiver listening on port 5000...\n";
@@ -28,9 +35,12 @@ int main()
     bool first = true;
 
     while (true)
-    {
+    {   
+        // recv blocks until data arrives
+        // then stores the data directly into the packet object
         recv(sock, (char*)&p, sizeof(p), 0);
 
+        // Detect packet loss
         if (!first && p.sequence_number != last_seq + 1)
         {
             std::cout
@@ -47,6 +57,6 @@ int main()
         first = false;
     }
 
-    closesocket(sock);
-    WSACleanup();
+    closesocket(sock); // Release socket resources
+    WSACleanup();      // Shuts down Winsock
 }
--- src/sender/main.cpp
+++ src/sender/main.cpp
@@ -1,23 +1,30 @@
 #include <iostream>
-#include <winsock2.h>
-#include <ws2tcpip.h>
-#include <chrono>
+#include <winsock2.h> // Socket API
+#include <ws2tcpip.h> // Networking: inet_pton: IPV4 --> binary ip
+#include <chrono>     // Generate timestamps
 #include <thread>
 
 #include "packet.hpp"
 
-#pragma comment(lib, "ws2_32.lib")
+#pragma comment(lib, "ws2_32.lib") // Link against the winsock library
 
 int main()
 {
     WSADATA wsaData;
+    // Winsock version 2.2
     WSAStartup(MAKEWORD(2, 2), &wsaData);
 
+    // Adress Family Internet/IPv4, Datagram socket/UDP, Default protocol
     SOCKET sock = socket(AF_INET, SOCK_DGRAM, 0);
 
+    // Define where we should send packets - Structure: IP Address + port
     sockaddr_in receiver{};
-    receiver.sin_family = AF_INET;
-    receiver.sin_port = htons(5000);
+    receiver.sin_family = AF_INET; // IPv4
+    // PC (little-endian), Network (big-endian). 
+    // htons: host to network short, 5000 -> network byte order
+    receiver.sin_port = htons(5000); 
+    // Converts IPv4 address into binary.
+    // pton: presentation to network: presentation(human-readable) --> binary
     inet_pton(AF_INET, "127.0.0.1", &receiver.sin_addr);
 
     uint32_t seq = 0;
@@ -32,12 +39,12 @@ int main()
             ).count();
 
         sendto(
-            sock,
-            (const char*)&p,
-            sizeof(p),
-            0,
-            (sockaddr*)&receiver,
-            sizeof(receiver)
+            sock,                 // Which socket to use
+            (const char*)&p,      // Treat p as raw bytes (naive serialization)
+            sizeof(p),            // Number of bytes to send.
+            0,                    // Flags, currently unused.
+            (sockaddr*)&receiver, // Destination address: 127.0.0.1:5000
+            sizeof(receiver)      // Size of the destination structure
         );
 
         std::cout << "Sent packet " << p.sequence_number << "\n";
@@ -45,6 +52,6 @@ int main()
         std::this_thread::sleep_for(std::chrono::milliseconds(500));
     }
 
-    closesocket(sock);
-    WSACleanup();
+    closesocket(sock); // Release socket resources
+    WSACleanup();      // Shuts down Winsock
 }
